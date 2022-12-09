ea123b122abafe42ec4fbea35af2c2f3f484a8da
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2022-12-09 17:14:42 +0800

    prikol5(Anton Clown)

--- src/Places/Utilits/Place.java
+++ src/Places/Utilits/Place.java
@@ -1,13 +1,16 @@
 package Places.Utilits;
 
+import Places.Capital;
+import Places.City;
+import Places.Region;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
 public abstract class Place {
-    public static Set<Place> places = new HashSet<Place>();
-    //интерфейсы
+    public int type;
     protected String name;
     public String getName() {
         return name;
@@ -19,6 +22,17 @@ public abstract class Place {
 
     public void myNameIs(){
         System.out.printf("Мое имя: %s\n", name);
+    }
+
+    public Set getSet(){
+        return (
+            switch (type) {
+                case 1 -> Region.places;
+                case 2 -> City.places;
+                case 3 -> Capital.places;
+                default -> throw new IllegalStateException("Unexpected value: " + type);
+            }
+        );
     }
 
     // Вложенный класс для взаимодействия
